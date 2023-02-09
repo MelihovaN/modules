eb7d9d8cbf1aa4d50592fbdfb6f15509c7a912d4
n.melihova <melihova.qa@gmail.com>
2023-02-09 22:02:52 +0200

    Changed readme file

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// const color = require("colors");
-
-// console.log(color.bgWhite("Colored results"));
-
 const calculateBonus = (a, b) => {
   let bonus;
   const sum = a + b;
-  debugger; /*на данный момент у нас присвоены значения для a и b, 
-  и посчитанна сумма этих значений*/
 
   sum > 50 ? (bonus = 50) : (bonus = sum);
-  debugger; /* выполненно условие - если сумма > 50, то бонус равен 50. 
-  Если это условие не выполняется (сумма< 50, то бонус будет равен сумме) */
-
   return bonus;
 };
-
-calculateBonus(60, 20);
+module.exports = calculateBonus;
+//module.exports = sum;
